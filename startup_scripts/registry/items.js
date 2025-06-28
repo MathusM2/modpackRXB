ea970fdf926481129c7f9b779bd23e683f400b72
Matias <superlima1212@gmail.com>
2025-06-27 22:17:33 -0300

    add new resources images for items parts tiers changed line item.js

--- startup_scripts/registry/items.js
+++ startup_scripts/registry/items.js
@@ -1,6 +1,59 @@
-StartupEvents.registry('item', event => {
-    event.create('blueprint_tierii')
-    .texture('/assets/kubejs/silentgear/textures/item/blueprinttier2.png')
-    .displayName('Blueprint Tier II')
+const toolsparts =[
+    "rod",
+    "tip",
+    "grip",
+    "binding",
+    "lining",
+    "cord",
+    "pickaxe",
+    "dagger",
+    "knife",
+    "trident",
+    "spear",
+    "machete",
+    "katana",
+    "sword",
+    "fletching",
+    "shovel",
+    "axe",
+    "paxel",
+    "hammer",
+    "excavator",
+    "saw",
+    "hoe",
+    "mattock",
+    "pospector",
+    "shield",
+    "slingshot",
+    "crossbow",
+    "bow",
+    "fishing_rod",
+    "shears",
+    "sickle",
+    "helmet",
+    "chestplate",
+    "leggings",
+    "boots",
+    "elytra",
+    "arrow",
+    "ring",
+    "bracelet",
+]
 
+
+
+StartupEvents.registry('item', event => {
+    event.create('blueprinttier2')
+    .displayName('Blueprint Tier (III)'),
+    event.create('blueprinttier3')
+    .displayName('Blueprint Tier (IV)')
+    
+    //partes em blue print:
+    toolsparts.forEach((part) =>{
+    event.create(`${part}_blueprinttier2`)
+    .displayName(`Tool ${part} Blueprint (II)`).texture('layer0','kubejs:item/blueprinttier2').texture('layer1',`kubejs:item/blueprint_${part}`)
+    event.create(`${part}_blueprinttier3`)
+    .displayName(`Tool ${part} Blueprint (II)`).texture('layer0','kubejs:item/blueprinttier3').texture('layer1',`kubejs:item/blueprint_${part}`)
+
+})
 })
